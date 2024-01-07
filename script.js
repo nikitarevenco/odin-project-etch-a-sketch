17263fd42492e4c4c46919feab5007ac6c358e29
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-01-07 19:31:47 +0000

    Align correctly each pixel on canvas
    All pixels on canvas automatically get aligned
    now have equal sides

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 let mainContainer = document.querySelector(".main-content");
+let getConverterWidth = window
+  .getComputedStyle(document.querySelector(".percentage-to-pixels"))
+  .getPropertyValue("width");
 
 // Select all cell-divs
+
+function extractNumbers(string) {
+  let matches = string.match(/(\d+)/);
+  if (matches) {
+    return +matches[0];
+  }
+}
 
 function createCellDivs(length) {
   for (let i = 0; i < length ** 2; i++) {
@@ -11,21 +21,32 @@ function createCellDivs(length) {
   }
 }
 
-function calculateSize(length) {
-  let size = 1 / length;
-  return size;
+function calculateWidth(length) {
+  let size = ((1 / length) * 100).toString() + "%";
+  return size; // Percentage occupied by one div
+}
+
+function calculateHeight(length) {
+  let heightWidth =
+    extractNumbers(calculateWidth(length)) *
+    extractNumbers(getConverterWidth) *
+    0.01;
+
+  return heightWidth;
 }
 
 function setDivStyle(length) {
   let allDivs = document.querySelectorAll(".cell-div");
-  console.log(allDivs);
   for (let i = 0; i < allDivs.length; i++) {
-    allDivs[i].style.width = "100px";
-    allDivs[i].style.height = "100px";
-    // Remove this later
-    allDivs[i].style.border = "3px solid red";
+    allDivs[i].style.width = `${calculateWidth(length)}`;
+    allDivs[i].style.height = `${calculateHeight(length)}px`;
+    allDivs[i].style.boxSizing = "border-box";
   }
 }
 
-createCellDivs(50);
-setDivStyle(50);
+function test(length = 16) {
+  createCellDivs(length);
+  setDivStyle(length);
+}
+
+test();
